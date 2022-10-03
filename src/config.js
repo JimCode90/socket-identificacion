ad81f93bf92d7cc53046772b837f2df4b0323884
JimCode <jimmypaytan10@gmail.com>
2022-10-03 10:22:55 -0500

    cambios ultimos sockets

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export const PORT = process.env.PORT || 3000;
 // MYSQL
 export const MYSQL_HOST = process.env.MYSQL_HOST || "localhost";
 export const MYSQL_USER = process.env.MYSQL_USER || "root";
-export const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || "";
+export const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || "root";
 export const MYSQL_DATABASE = process.env.MYSQL_DATABASE || "bd_projectdemo";
 export const MYSQL_PORT = process.env.MYSQL_PORT || 3306;
 
